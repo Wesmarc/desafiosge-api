77f4e307be86cc4468be7f609676b92e8e6cac52
weslei.nonato <weslei.nonato@sgetec.com.br>
2022-08-28 10:31:56 -0300

    Atualização no projeto Blog

--- desafiosge-api/src/main/java/com/sgebackend/blog/desafiosgeapi/controller/SpringBootController.java
+++ desafiosge-api/src/main/java/com/sgebackend/blog/desafiosgeapi/controller/SpringBootController.java
@@ -5,11 +5,15 @@ import com.sgebackend.blog.desafiosgeapi.model.Post;
 import com.sgebackend.blog.desafiosgeapi.service.DesafiosgeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -41,7 +45,16 @@ public class SpringBootController {
     public String getPostForm() {
         return "postForm";
     }
-
+    @RequestMapping(value = "/newpost", method = RequestMethod.POST)
+    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
+        attributes.addFlashAttribute("messagem", "Verifique se os campos obrigatórios foram preenchidos!");
+        if(result.hasErrors()) {
+            return "redirect:/newpost";
+        }
+        post.setData(LocalDate.now());
+        desafiosgeService.save(post);
+        return "redirect:/posts";
+    }
 
 
 
